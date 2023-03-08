e3c0e847b6d8fdf261da6c9921e03594f2c21f6a
Nhat Hoang <johnhoanghtx@gmail.com>
2023-03-08 15:43:16 -0600

    Origin

---
+++ LeetCode.js
@@ -0,0 +1,26 @@
+// Given the root of a binary tree, invert the tree, and return its root.
+
+// Input: root = [4,2,7,1,3,6,9]
+// Output: [4,7,2,9,6,3,1]
+
+// Input: root = [2,1,3]
+// Output: [2,3,1]
+
+// Input: root = []
+// Output: []
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+    
+};
